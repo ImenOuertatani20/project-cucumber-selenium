28cc7652cfb0ac7d15f7a544897f170b94f3f40a
ImenOuertatani20 <imen.ouertatani@esprit.tn>
2021-01-31 10:19:03 +0100

    update project

---
+++ src/test/java/com/orangeHRM/e2eTests/utils/DriverType.java
@@ -0,0 +1,6 @@
+package com.orangeHRM.e2eTests.utils;
+
+
+public enum DriverType {
+	CHROME, FIREFOX, IE, EDGE;
+}
---
+++ target/cucumber-html-report/report.js
@@ -0,0 +1,82 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("authentification/Authentification.feature");
+formatter.feature({
+  "line": 2,
+  "name": "Authentification - OrangeHRM",
+  "description": "En tant que utilisateur je souhaite m\u0027authentifier",
+  "id": "authentification---orangehrm",
+  "keyword": "Feature",
+  "tags": [
+    {
+      "line": 1,
+      "name": "@connexion"
+    }
+  ]
+});
+formatter.before({
+  "duration": 30185188500,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 6,
+  "name": "Authentification - OrangeHRM",
+  "description": "",
+  "id": "authentification---orangehrm;authentification---orangehrm",
+  "type": "scenario",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "line": 5,
+      "name": "@cnx"
+    }
+  ]
+});
+formatter.step({
+  "line": 7,
+  "name": "Je me connecte à l\u0027application OrangeHRM",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 8,
+  "name": "Je saisi Username",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 9,
+  "name": "Je saisi Password",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 10,
+  "name": "Je click sur le bouton login",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 11,
+  "name": "Redirection vers le compte admin",
+  "keyword": "Then "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.after({
+  "duration": 8715461900,
+  "status": "passed"
+});
+});
